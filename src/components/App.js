5351c87e75e02b9bca85ebd7f03db89d95198bc6
David <dp8012@pleasantonusd.net>
2021-05-20 23:33:58 -0700

    dfbZfbafghfahb

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import '../Css/App.css';
+import Note from "./Note"
+
+function App() {
+    return (
+        <div className = "app">
+            <Note/>
+        </div>
+    );
+}
+
+export default App;
